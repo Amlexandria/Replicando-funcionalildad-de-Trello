d83f7b1e3c160ee0c5de8d3cf938b6207a01e6e4
Amlexandria <antestumejorsonrisa@gmail.com>
2018-05-15 14:35:00 -0500

    Se agrega la version 2 del ejercicio.

--- js/app.js
+++ js/app.js
@@ -12,10 +12,12 @@ toAddAList.addEventListener("click", function(){
   var divform2 = document.createElement("div");
 
   //Creando el input
-  var inputNameOtTheList = document.createElement("input");
-  inputNameOtTheList.id = "name-list";
-  inputNameOtTheList.setAttribute("class","text-list");
-  inputNameOtTheList.setAttribute("placeholder","Add a list...")
+  var inputNameOfTheList = document.createElement("input");
+  inputNameOfTheList.id = "name-list";
+  inputNameOfTheList.setAttribute("class","text-list");
+  inputNameOfTheList.setAttribute("placeholder","Add a list...");
+  inputNameOfTheList.setAttribute("autofocus", "");
+
 
   //Creando el botón
   var saveButton = document.createElement("button");
@@ -24,11 +26,48 @@ toAddAList.addEventListener("click", function(){
   saveButton.setAttribute("type", "button");
   saveButton.innerText = "Save";
   //Añadiendo Elementos
-  divform1.appendChild(inputNameOtTheList);
+  divform1.appendChild(inputNameOfTheList);
   divform2.appendChild(saveButton);
   form.appendChild(divform1);
   form.appendChild(divform2);
   divaddAListContainer.appendChild(form);
 
+
+
+
+
+  saveButton.addEventListener('click', function(){
+    //Creando el Div de la Lista creada
+    var divList = document.createElement("div");
+    divList.id = "div-of-the-List";
+    divList.className = "container width-list-elements text-list";
+
+    var innerdivList1 = document.createElement("div");
+    innerdivList1.id = "div1ofTheList";
+    var innerdivList2 = document.createElement("div");
+
+    //Creando el parrafo que con el nombre de la Lista
+    var nameOfTheList = document.createElement("p");
+    nameOfTheList.textContent= inputNameOfTheList.value;
+    document.getElementById("name-list").value = "";
+
+
+    //Creando en enlace Add a Card...
+    var linkAddACard = document.createElement("a");
+    linkAddACard.innerText = "Add a card..."
+    linkAddACard.classList.add("text-list","width-list-elements");
+
+
+    var contenedorDeVersiones = document.getElementById("contenedor-de-versiones");
+    //Agregando elementos
+    innerdivList1.appendChild(nameOfTheList);
+    innerdivList2.appendChild(linkAddACard);
+    divList.appendChild(innerdivList1);
+    divList.appendChild(innerdivList2);
+    contenedorDeVersiones.appendChild(divList);
+    //Insertando a divList antes de divaddAListContainer
+    contenedorDeVersiones.insertBefore(divList, divaddAListContainer);
+
+  });
 
 });
---
+++ js/index.js
@@ -0,0 +1,1 @@
+window.saveButton = saveButton;
